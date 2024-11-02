4b826759e8a9c354fa335539039365b7c6de872b
Abhay <abhaysharma.cs2017.pgdav@gmail.com>
2024-11-02 11:51:26 +0530

    fixing code to pass the linting

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
-def add(a,b):
+def add(a, b):
     return a+b
-
-
-
 
 
 def sub(a,b):
     return a-b
 
 
-sum = add(4,5)
-substract = sub(5,4)
+sum = add(4, 5)
+substract = sub(5, 4)
 
 
 print(sum, substract)
